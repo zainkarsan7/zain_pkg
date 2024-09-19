2d77f83d841db73e20da46d501496034705527c2
zainkarsan7 <zainkarsan7@gmail.com>
2024-09-19 14:12:59 +0000

    fixed

--- launch/zains_launch.py
+++ launch/zains_launch.py
@@ -1,11 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from moveit_configs_utils import MoveItConfigsBuilder
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_moveit_configs()
-    
-    
+   
     tut_node = Node(
         package="zain_pkg",
         executable="zain_robot",
--- src/robot_moveit.cpp
+++ src/robot_moveit.cpp
@@ -2,21 +2,54 @@
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
 
-int main (int argc, char *argv []){
+int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
-    rclcpp::NodeOptions node_options;
-    node_options.allow_undeclared_parameters(true);
-    auto node = rclcpp::Node::make_shared("zains_robot",node_options);
-
+    rclcpp::NodeOptions node_opts;
+    node_opts.automatically_declare_parameters_from_overrides(true);
+    auto node = rclcpp::Node::make_shared("rob_loader",node_opts);
     const auto& LOGGER = node->get_logger();
 
 
-    robot_model_loader::RobotModelLoader rml(node);
-    const moveit::core::RobotModelPtr& kin_model = rml.getModel();
-    RCLCPP_INFO(LOGGER,"Model frame : %s", kin_model->getModelFrame().c_str());
+
+    // load robot given params from the launch.py
+    robot_model_loader::RobotModelLoader robot_loader(node);
+    const moveit::core::RobotModelPtr& k_model = robot_loader.getModel();
+    std::vector<std::string> names = k_model->getJointModelGroupNames();
+    for (std::size_t i=0; i<names.size(); ++i){
+        RCLCPP_INFO(LOGGER,"model has these groups  %s",names[i].c_str());
+    }
     
-    moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(kin_model));
-    robot_state->setToDefaultValues();
+    const moveit::core::JointModelGroup* joint_model_group = k_model->getJointModelGroup("panda_arm");
 
+    const std::vector<std::string>& joint_names = joint_model_group->getVariableNames();
+    
+    moveit::core::RobotStatePtr rob_state(new moveit::core::RobotState(k_model));
+    rob_state->setToDefaultValues();
+    rob_state->setToRandomPositions(joint_model_group);
+    std::vector<double> j_vals;
+    rob_state->copyJointGroupPositions("panda_arm",j_vals);
+    for (std::size_t i=0;i<joint_names.size();++i){
+        RCLCPP_INFO(LOGGER, "these are the j values for %s: %f",joint_names[i].c_str(),j_vals[i]);
+    }
+
+    const Eigen::Isometry3d& eef_state = rob_state->getGlobalLinkTransform("panda_link8");
+    RCLCPP_INFO_STREAM(LOGGER,"Translation: \n"<<eef_state.translation()<<"\n");
+    RCLCPP_INFO_STREAM(LOGGER,"Rotation: \n"<<eef_state.rotation()<<"\n");
+
+    double timeout = 0.1;
+    rob_state->setToDefaultValues();
+    bool found_ik = rob_state->setFromIK(joint_model_group,eef_state,timeout);
+
+    if (found_ik){
+        rob_state->copyJointGroupPositions(joint_model_group,j_vals);
+        for (std::size_t i=0;i<joint_names.size();++i){
+            RCLCPP_INFO(LOGGER, "joint %s, %f",joint_names[i].c_str(),j_vals[i]);
+        }
+    }else{
+        RCLCPP_INFO(LOGGER,"couldnt find soln");
+    }
+
+
+    rclcpp::shutdown();
+    return 0;
 }
-
